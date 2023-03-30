f16024b35b5ea0d6ad1491e68c3b9b1285bc9da6
Dany <daniela.ruizgar1@gmail.com>
2023-03-29 23:37:08 -0700

    feat: intro to sequelizer

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express') // recibe la libreria express, las funciones se llaman con parentesis
+const db = require('./src/models')
 const morgan = require('morgan')
 
 import apiRouter from './src/routes';
 
 const app = express() // inicializa el express(), le devuelve el servidor a app
 const port = 3030 // el puerto en el que se va a levantar
+
+db.sequelize.sync()
+ .then(() => {
+ console.log("Synced db.");
+ })
+ .catch((err) => {
+ console.log("Failed to sync db: " + err.message);
+ });
+
+/* db.sequelize.sync({ force: true }).then(() => {
+console.log("Drop and re-sync db.");
+}); */
 
 app.use(morgan('dev')) // usa la aplicacion morgan con el minimo de developer
 app.use(express.json());
